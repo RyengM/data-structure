2b8099086cb0fda4cae3588250b0b3a09a1f9dff
demonhub <834244710@qq.com>
2017-05-30 23:39:00 +0800

    temp

--- Lab4.c
+++ Lab4.c
@@ -7,11 +7,54 @@ typedef struct Bnode{
     struct Bnode *Lchild,*Rchild;
 }BTnode,*BTptr;
 
-void CreatLBtree(BTptr BT,char word[][],int k){//建立排序二叉树
+typedef struct node {
+	BTptr *next;
+}*stack;
+
+stack Pop(stack *top) {
+	stack p,e;
+	if (*top == NULL) return NULL;
+	else {
+		strcpy(e->data,(*top)->data);
+		p = (*top);
+		*top = (*top)->next;
+		free(p);
+		return e;
+	}
+}
+
+void Push(stack *top, BTptr e) {
+	stack p;
+	p = (stack)malloc(sizeof(snode));
+	strcpy(p->data,e->data);
+	p->next = *top;
+	*top = p;
+}
+
+void ClearStack(stack *top) {
+	stack p;
+	while (*top != NULL) {
+		p = (*top)->next;
+		Pop(top);
+		*top = p;
+	}
+	*top = NULL;
+}
+
+int EmptyStack(stack S) {
+	if (S == NULL) return 1;
+	else return 0;
+}
+
+stack GetsTop(stack S) {
+	if (S != NULL) return S;
+	return (0);
+}
+void CreatLBtree(BTptr BT,char word[][20],int k){//建立排序二叉树
     BTptr p,q;
     int i = 0;
     while (k--){
-        p = (BTptr)malloc(sizeof(BTndoe));
+        p = (BTptr)malloc(sizeof(BTnode));
         strcpy(p->data,word[i]);
         p->Lchild = p->Rchild = NULL;
         i++;
@@ -24,7 +67,7 @@ void CreatLBtree(BTptr BT,char word[][],int k){//建立排序二叉树
                     else {q->Lchild = p;break;}
                 }
                 else if (strcmp(p->data,q->data)>0){
-                    if (q->Rchlid != NULL) q = q->Rchild;
+                    if (q->Rchild != NULL) q = q->Rchild;
                     else {q->Rchild = p;break;}
                 }
                 else break;//查找到相同数据，无需插入
@@ -33,8 +76,27 @@ void CreatLBtree(BTptr BT,char word[][],int k){//建立排序二叉树
     }
 }
 
+void visit(BTptr p){
+    printf("%s\t",p->data);
+}
+
 void Inorder(BTptr T){//进行中序遍历
-    BTptr p;
+    BTptr p,q;
+    stack s;
+    ClearStack(s);
+    Push(s,T);
+    while (!EmptyStack(s));{
+        while (p!=NULL){
+            Push(s,p->Lchild);
+            p = p->Lchild;
+        }
+        p = Pop(s);
+        if (!EmptyStack(s)){
+            p = Pop(s);
+            visit(p);
+            Push(s,p->Rchild);
+        }
+    }
 }
 
 void main(){
@@ -44,10 +106,11 @@ void main(){
     int i=0,j=0;
     while(1){
         scanf("%c",&word[i][j]);
-        if (word[i][j] == ' ') {i++; j=0;}
+        if (word[i][j] == ' ') {word[i][j]='\0';i++; j=0;}
         else if (word[i][j] == '\n') break;
         else j++;
     }
     CreatLBtree(BT,word,i+1);
+    Inorder(BT);
     system("pause");
 }
